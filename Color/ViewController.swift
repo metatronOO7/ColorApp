01711cf35da6340499c013d62253a5a0d1b3d88c
hendoware <hendoware@gmail.com>
2018-10-03 12:17:25 -0400

    Initial Commit

---
+++ Color/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color
+//
+//  Created by Sean Hendrix on 10/3/18.
+//  Copyright © 2018 Sean Hendrix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
